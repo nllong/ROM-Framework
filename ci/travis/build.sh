d9d3dbc2f849bf8f4dc8a23aeb9116cf77f85b99
Nicholas Long <nicholas.long@nrel.gov>
2018-03-23 12:06:25 -0600

    all models

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -7,15 +7,18 @@ if [ "${BUILD_SCOPE}" == "all" ]; then
     cd results && bundle exec ruby post_process.rb --post-process && cd ..
 
     echo "Building Regression Models"
-#    analyses=(results/*/)
-#    for dir in "${analyses[@]}"
-#    do
-#        analysis_id=$(basename ${dir})
-#        echo "Processing ${analysis_id}"
-#
-#        cd python && python build_models.py -a ${analysis_id} && cd ..
-#    done
 
-     cd python && python build_models.py -a 5564b7d5-4def-498b-ad5b-d4f12a463275 && cd ..
+    # Uncomment the below to build all the models
+    analyses=(results/*/)
+    for dir in "${analyses[@]}"
+    do
+        analysis_id=$(basename ${dir})
+        echo "Processing ${analysis_id}"
+
+        cd python && python build_models.py -a ${analysis_id} && cd ..
+    done
+
+    # One off builds using ID number
+#     cd python && python build_models.py -a 5564b7d5-4def-498b-ad5b-d4f12a463275 && cd ..
 
 fi
