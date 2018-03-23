ed9f61960fc24777ac2ede4498ae9ef8037ad1a0
Nicholas Long <nicholas.long@nrel.gov>
2018-03-23 10:31:04 -0600

    remove user

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -4,12 +4,18 @@ BUILD_SCOPE=$1
 
 if [ "${BUILD_SCOPE}" == "all" ]; then
     echo "Post Processing Results"
-    cd results
-    ruby post_process.rb --post-process
-    cd ..
+    cd results && bundle exec ruby post_process.rb --post-process && cd ..
 
     echo "Building Regression Models"
-    cd python
-    python build_models.py -a 5564b7d5-4def-498b-ad5b-d4f12a463275
-    cd ..
+#    analyses=(results/*/)
+#    for dir in "${analyses[@]}"
+#    do
+#        analysis_id=$(basename ${dir})
+#        echo "Processing ${analysis_id}"
+#
+#        cd python && python build_models.py -a ${analysis_id} && cd ..
+#    done
+
+     cd python && python build_models.py -a 5564b7d5-4def-498b-ad5b-d4f12a463275 && cd ..
+
 fi
--- ci/travis/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd python && pip install -r requirements.txt && cd ..
-cd results && bundle install && cd ..
-
