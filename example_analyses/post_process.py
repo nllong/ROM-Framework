1bbea38279911d5d827b66cc6bcac05dc33cfc1a
Nicholas Long <nicholas.long@nrel.gov>
2018-09-15 17:05:33 -0600

    rename outputs

--- example_analyses/post_process.py
+++ example_analyses/post_process.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-df = pd.read_csv('output.csv')
+df = pd.read_csv('example_output/output.csv')
 df['datetime'] = df['datetime'].astype('datetime64[m]')
 df['DayOfYear'] = df['datetime'].dt.dayofyear
 
-output_dir = 'example_analyses/example_output'
+output_dir = 'example_output'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -23,5 +23,6 @@ for response in responses:
     heatdata = df[["DayOfYear", "Hour", response]].pivot("DayOfYear", "Hour", response)
     f, ax = plt.subplots(figsize=(5, 12))
     sns.heatmap(heatdata)
-    plt.savefig('%s/%s.png' % (output_dir,response))
+    filename = '%s/%s.png' % (output_dir, response.replace(' ', '_'))
+    plt.savefig(filename)
     plt.close('all')
